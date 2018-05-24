5c68bb8dd9913e52230f2c710ca453dfbe591069
Георгий Зубков <allengzixet@gmail.com>
2018-05-24 13:55:51 +0300

    Первый коммит

---
+++ AnimationProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AnimationProject
+//
+//  Created by Георгий Зубков on 24.05.2018.
+//  Copyright © 2018 Георгий Зубков. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AnimationProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AnimationProject
+//
+//  Created by Георгий Зубков on 24.05.2018.
+//  Copyright © 2018 Георгий Зубков. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
